d67047e1afe5a0ffc17b09df269b20d7b8ab40ed
faridfg <faridfg@code.edu.az>
2020-04-06 07:42:15 +0400

    account page layout added

---
+++ SciCo FrontEnd/js/account.js
@@ -0,0 +1,30 @@
+$(function() {
+    // incoming and logout section starts
+    var buttons = document.getElementsByClassName("button");
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener("click", function(ev) {
+            ev.preventDefault();
+            if (($(this).nextAll(".subMenu").hasClass("show")) !== true) {
+                $(".show").removeClass("show");
+                $(this).nextAll(".subMenu").addClass("show");
+            } else {
+                $(this).nextAll(".subMenu").removeClass("show");
+            }
+        })
+    }
+    // incoming and logout section ends
+
+    // left aside section starts
+    $("aside.left > div").hover(function() {
+        $(this).css({
+            "transform": "translateX(0)",
+            "transition": "transform .3s"
+        });
+    }, function() {
+        $(this).css({
+            "transform": "translateX(-70%)",
+            "transition": "transform .4s"
+        });
+    });
+    // left aside section ends
+})
