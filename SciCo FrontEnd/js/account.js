1f5040d72ef442a5c2a1f49b5ed52522c775a46f
faridfg <faridfg@code.edu.az>
2020-04-22 23:37:13 +0400

    update profile and cover photo views optimized

--- SciCo FrontEnd/js/account.js
+++ SciCo FrontEnd/js/account.js
@@ -166,4 +166,27 @@ $(function() {
         $("#cPhoto").click();
     })
     // updating profile and cover photos ends
-})
+
+    var container = document.querySelector("#pictureBoxCover > div:nth-child(3)");
+    var photo = document.querySelector("#pictureBoxCover form input[type='file']");
+    console.log(container)
+    console.log(photo)
+    photo.addEventListener("change", readURL(this));
+    })
+
+
+
+    
+
+ function readURL(input){
+    var ext = input.files[0]['name'].substring(input.files[0]['name'].lastIndexOf('.') + 1).toLowerCase();
+    if (input.files && input.files[0] && (ext == "gif" || ext == "png" || ext == "jpeg" || ext == "jpg")) 
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        $('#img').attr('src', e.target.result);
+    }
+    reader.readAsDataURL(input.files[0]);
+    else{
+        $('#img').attr('src', '/assets/no_preview.png');
+    }
+}
